e4931f5d521d08109dd88c2a1c359f14cfa7f76a
Nata-Kalina <kalinichevanata888@gmail.com>
2023-12-21 14:58:17 -0500

    Fixed orders spec

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -43,8 +43,8 @@ RSpec.describe "Orders", type: :request do
       order = FactoryBot.create(:order)
       order_attributes = FactoryBot.attributes_for(:order, customer_id: customer.id)
       expect { post orders_path, params: {order: order_attributes}
-    }.to change(Order, :count)
-      expect(response).to redirect_to order_path(id: order.id)
+    }.to change(Order, :count).by(1)
+      expect(response).to redirect_to order_path(Order.last)
     end
   end
 
